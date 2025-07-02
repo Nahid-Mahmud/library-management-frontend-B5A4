ee7a201c72b0618184b7e4cb8c34e0b3f88c40b4
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-02 20:04:08 +0600

    feat: add initial project setup with React, Vite, and Tailwind CSS
    - Added SVG assets for Vite and React.
    - Implemented a reusable Button component with variants.
    - Created a MainLayout component for routing.
    - Set up routing with React Router.
    - Added a Home page with a simple greeting.
    - Configured Tailwind CSS and custom CSS variables.
    - Established TypeScript configuration for the project.
    - Integrated Vite for development and build processes.

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
