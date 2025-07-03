77c4914ff3f466d682c60b1554bb03d0e43ae4b2
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-03 18:18:34 +0600

    feat: add ToastContainer to main.tsx for notification support

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import "./index.css";
 import router from "./routes/index.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store/index.ts";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
+      <ToastContainer position="top-right" autoClose={3000} />
     </Provider>
   </StrictMode>
 );
