6cc54ecb048208035888238d569bb25224222b47
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-03 11:47:11 +0600

    feat: replace placeholder content with HeroSection component in Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/home/HeroSection";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Button</Button>
+      <HeroSection />
     </div>
   );
 }
